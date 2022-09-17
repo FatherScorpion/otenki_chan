55d70db96b04a8f8f2ea9c6cb0b8d9408186dbc7
FatherScorpion <FatherScorpion@outlook.jp>
2022-09-17 22:20:54 +0900

    sheIsNowSpeak

--- lib/main.dart
+++ lib/main.dart
@@ -45,10 +45,18 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void addOpponentChatLog(List<double> tmp, List<double> pre, List<double> slr){
+    String text="hoge";
+    text="今の気温は"+tmp[0].toStringAsFixed(1)+"度だよ\nちなみに降水量は"+pre[0].toStringAsFixed(1)+"mmだね";
+    setState(() {
+      chatLogs.insert(0,{'isMine': false, 'text': text, 'favRate':1});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: OtenkiAppbar(addChatLog: addChatLog),
+      appBar: OtenkiAppbar(addChatLog: addChatLog, addOpponentChatLog: addOpponentChatLog),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
