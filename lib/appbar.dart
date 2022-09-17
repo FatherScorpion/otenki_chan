50e4b347c0bea7e46b924c0cec43bf5873f0ec7c
MiniSlime <takuminn717@gmail.com>
2022-09-18 07:12:50 +0900

    [update]初回天気取得時に権限を要求するように

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -249,6 +249,27 @@ Future<String> getWeatherToken() async{
 }
 
 Future<List<double>> getTemperatureData(String token) async{
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
   double lat,lng;
   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
   lat=position.latitude;
